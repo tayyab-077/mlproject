98787c7956f45c6bda3b78e38194ca65f5c5eaf8
tayyab07goal@gmail.com <tayyab07goal@gmail.com>
2024-11-28 11:12:51 +0530

    changes in stylesheet

--- application.py
+++ application.py
@@ -42,7 +42,10 @@ def predict_datapoint():
         print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
         print("after Prediction")
-        return render_template('home.html',results=results[0])
+        formatted_result = "{:.2f}".format(results[0])
+        return render_template('home.html', results=formatted_result)
+
+        #return render_template('home.html',results=results[0])
     
 
 if __name__=="__main__":
